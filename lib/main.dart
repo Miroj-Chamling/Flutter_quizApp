4a5f8035c5b50294999d899febf93f70d083ca33
miroj-lovesmomo <81049151+miroj-lovesmomo@users.noreply.github.com>
2021-08-06 21:04:05 +0545

    updated on score

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,9 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   //final is used for a value that is first inititalied and is changed during the run time;
   //const is a value that wont be changed through out the run time.
+  var _indexNumber = 0;
+  var _totalscore = 0;
+
   final _questions = const [
     /*here {} is used to make a map we can even use map() function to create 
     a map but {} is a short way to create a map*/
@@ -30,27 +33,46 @@ class _MyAppState extends State<MyApp> {
     */
     {
       'questionText': 'what is your favourite fruit?',
-      'answer': ['apple', 'banana', 'grape', 'mango'],
+      'answers': [
+        {'text': 'apple', 'score': 10},
+        {'text': 'banana', 'score': 5},
+        {'text': 'grape', 'score': 7},
+        {'text': 'mango', 'score': 8},
+      ],
     },
     {
       'questionText': 'what is your favourite sport?',
-      'answer': ['football', 'chess', 'basketball', 'cricket'],
+      'answers': [
+        {'text': 'football', 'score': 10},
+        {'text': 'chess', 'score': 10},
+        {'text': 'basketball', 'score': 10},
+        {'text': 'cricket', 'score': 10},
+      ],
     },
     {
       'questionText': 'what is your favourite song?',
-      'answer': ['kutu ma kutu', 'night changes', 'yatri', 'simsime pani'],
+      'answers': [
+        {'text': 'kutu ma kutu', 'score': 10},
+        {'text': 'night changes', 'score': 10},
+        {'text': 'yatri', 'score': 15},
+        {'text': 'simsime pani', 'score': 10},
+      ],
     },
     {
       'questionText': 'what is your favourite color?',
-      'answer': ['red', 'blue', 'green', 'pink'],
+      'answers': [
+        {'text': 'red', 'score': 10},
+        {'text': 'blue', 'score': 10},
+        {'text': 'green', 'score': 10},
+        {'text': 'pink', 'score': 10},
+      ],
     },
   ];
   //var dummy = ['miroj'];
   //dummy.add('Rai');
   //Print(dummy);
-  var _indexNumber = 0;
-
-  void _questionAnswer() {
+  void _questionAnswer(int score) {
+    _totalscore += score;
     setState(() {
       _indexNumber = _indexNumber + 1;
     });
